7c6e4572d682f63038bd3351e174b2ac22761a9e
karina-rodriguez <karminer60@gmail.com>
2020-05-20 09:38:30 -0700

    added new roles

--- src/teammateForm.js
+++ src/teammateForm.js
@@ -9,7 +9,7 @@ export default function TeammateForm(props) {
   } = props
 
   return (
-   
+    
     <form className='form container' onSubmit={onSubmit}>
       <div className='form-group submit'>
         <h2>Add a Teammate</h2>
@@ -19,22 +19,20 @@ export default function TeammateForm(props) {
       <div className='form-group inputs'>
         <h4>General information</h4>
 
-        
         <label>Username:&nbsp;
-        
-          <input
+     
             type='text'
             placeholder='Type a username'
-            maxLength='100'
+            maxLength='20'
             name='username'
-           
+            
             value={values.username}
             onChange={onInputChange}
           />
         </label>
 
         <label>Email:&nbsp;
-         
+          
           <input
             type='text'
             placeholder='Type email'
@@ -47,13 +45,13 @@ export default function TeammateForm(props) {
 
        
         <label>Role:&nbsp;
-          
+         
           <select name='role' value={values.role} onChange={onInputChange}>
               <option value='' >Select a Role</option>
-              <option value='Student'>Student</option>
-              <option value='Instructor'>Instructor</option>
-              <option value='Alumni'>Alumni</option>
-              <option value='TL'>TL</option>
+              <option value='UI Developer'>UI Developer</option>
+              <option value='Frontend Developer'>Frontend Developer</option>
+              <option value='Backend Developer'>Backend Developer</option>
+              <option value='Data Scientist'>Data Scientist</option>
           </select>
         </label>
       </div>
